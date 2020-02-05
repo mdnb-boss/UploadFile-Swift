213f601470cb676cd5bbf05a985eb5eedb850ff9
Marcelo Daniel <marcelodaniel.daniel@icloud.com>
2020-02-05 17:50:50 -0300

    initial commit

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '10.0'
+
+target 'UploadFiles' do
+  use_frameworks!
+  
+  pod 'Alamofire'
+end
---
+++ Pods/Target Support Files/Pods-UploadFiles/Pods-UploadFiles-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UploadFiles : NSObject
+@end
+@implementation PodsDummy_Pods_UploadFiles
+@end
